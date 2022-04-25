8b99b0ac350a62d201f48f77ed5e24b586d93c19
Svetlana Fedorenko <svetlana493@ukr.net>
2022-04-25 12:37:00 +0300

    зміни

--- js/task-01.js
+++ js/task-01.js
@@ -7,3 +7,18 @@ for (const element of categories) {
     console.log("\nCategory:", element.firstElementChild.textContent);
     console.log("Elements:", element.lastElementChild.children.length);
 }
+
+// const categoriesList = document.querySelectorAll("h2").length;
+// console.log("Numders of сategories", categoriesList);
+// const animalsTitle = document.querySelector("h2").textContent;
+// console.log("Category", animalsTitle);
+// const animalsList = document.querySelector(".item > ul").children.length;
+// console.log("Elements", animalsList);
+// const productsTitle = document.querySelectorAll(".item > h2")[1].textContent;
+// console.log("Category", productsTitle);
+// const productsList = document.querySelectorAll(".item > ul")[1].children.length;
+// console.log("Elements", productsList);
+// const technologiesTitle = document.querySelectorAll(".item > h2")[2].textContent;
+// console.log("Category", technologiesTitle);
+// const technologiesList = document.querySelectorAll(".item > ul")[2].children.length;
+// console.log("Elements", technologiesList);
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,3 @@
-// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input)
-// и изменяет инлайн - стиль span#text обновляя свойство font - size.
-// В результате при перетаскивании ползунка будет меняться размер текста.
-
 const controlInput = document.querySelector("#font-size-control");
 const controlText = document.querySelector("#text");
 
